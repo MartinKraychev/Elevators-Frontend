4887a17843bf631fa5078c319b84f5985059d3e8
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2023-10-25 12:13:01 +0100

    Added app structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { Routes, Route } from "react-router-dom"
+import { Header } from "./components/header/header";
+import { Config } from "./components/config/config";
+import { ElevatorControls } from "./components/elevator-controls/elevator-controls";
+import { ElevatorStatuses } from "./components/elevator-statuses/elevator-statuses";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Routes>
+				<Route path="/config" element={<Config />}></Route>
+				<Route path="/controls" element={<ElevatorControls />}></Route>
+				<Route path="/statuses" element={<ElevatorStatuses />}></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
